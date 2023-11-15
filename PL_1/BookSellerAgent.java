9c704d7ea2d3da0b32625fae67273ad11d312c1e
Rip-Hunter <71773661+Rip-Hunter@users.noreply.github.com>
2023-11-16 01:51:08 +0200

    Add files via upload

---
+++ PL_1/BookSellerAgent.java
@@ -0,0 +1,134 @@
+package main_files;
+
+import jade.core.Agent;
+import jade.core.behaviours.*;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
+import jade.domain.DFService;
+import jade.domain.FIPAException;
+// import jade.domain.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+
+import java.util.Hashtable;
+
+public class BookSellerAgent extends Agent {
+	// The catalogue of books for sale (maps the title of a book to its price)
+	
+	private Hashtable<String, Integer> catalogue;
+	// private Hashtable catalogue;
+	
+	// The GUI by means of which the user can add books in the catalogue
+	private BookSellerGui myGui;
+	// Put agent initializations here
+
+
+	private class OfferRequestsServer extends CyclicBehaviour {
+		public void action() {
+			MessageTemplate mt = MessageTemplate.MatchPerformative(ACLMessage.CFP);
+			ACLMessage msg = myAgent.receive(mt);
+			if (msg != null) {
+			// Message received. Process it
+				String title = msg.getContent();
+				ACLMessage reply = msg.createReply();
+				Integer price = catalogue.get(title);
+				if (price != null) {
+					// The requested book is available for sale. Reply with the price
+					reply.setPerformative(ACLMessage.PROPOSE);
+					reply.setContent(String.valueOf(price.intValue()));
+				}
+				else {
+					// The requested book is NOT available for sale.
+					reply.setPerformative(ACLMessage.REFUSE);
+					reply.setContent("not-available");
+				}
+				myAgent.send(reply);
+			}
+			else {
+				block();
+			} 
+		}
+	} // End of inner class OfferRequestsServer
+
+	private class PurchaseOrdersServer extends CyclicBehaviour {
+        public void action() {
+            MessageTemplate mt = MessageTemplate.MatchPerformative(ACLMessage.ACCEPT_PROPOSAL);
+            ACLMessage msg = myAgent.receive(mt);
+            if (msg != null) {
+                // Message received. Process it
+                String title = msg.getContent();
+                ACLMessage reply = msg.createReply();
+                Integer price = catalogue.remove(title);
+                if (price != null) {
+                    // Book sold. Reply with the confirmation
+                    reply.setPerformative(ACLMessage.INFORM);
+                    System.out.println(title + " sold to agent " + msg.getSender().getName());
+                } else {
+                    // Book not available
+                    reply.setPerformative(ACLMessage.FAILURE);
+                    reply.setContent("not-available");
+                }
+                myAgent.send(reply);
+            } else {
+                block();
+            }
+        }
+    }
+
+	protected void setup() {
+	// Create the catalogue
+		catalogue = new Hashtable<String, Integer>();
+		// Create and show the GUI
+		myGui = new BookSellerGui(this);
+		myGui.show();
+		// Add the behaviour serving requests for offer from buyer agents
+		addBehaviour(new OfferRequestsServer());
+		// Add the behaviour serving purchase orders from buyer agents
+		addBehaviour(new PurchaseOrdersServer());
+
+
+
+		// Register the book-selling service in the yellow pages
+ 		DFAgentDescription dfd = new DFAgentDescription();
+ 		dfd.setName(getAID());
+ 		ServiceDescription sd = new ServiceDescription();
+ 		sd.setType("book-selling");
+ 		sd.setName("JADE-book-trading");
+ 		dfd.addServices(sd);
+ 		try {
+ 			DFService.register(this, dfd);
+ 		}
+ 		catch (FIPAException fe) {
+ 			fe.printStackTrace();
+ 		}
+
+	}
+	// Put agent clean-up operations here
+
+	protected void takeDown() {
+
+		// Deregister from the yellow pages
+		try {
+			DFService.deregister(this);
+		}
+		catch (FIPAException fe) {
+			fe.printStackTrace();
+		}
+
+		// Close the GUI
+		myGui.dispose();
+		// Printout a dismissal message
+		System.out.println("Seller-agent "+getAID().getName()+" terminating.");
+	}
+ 	/**
+ 	This is invoked by the GUI when the user adds a new book for sale
+ 	*/
+ 	public void updateCatalogue(final String title, final int price) {
+ 		addBehaviour(new OneShotBehaviour() {
+ 			public void action() {
+ 				catalogue.put(title,price);
+ 				System.out.println(title + " inserted into catalogue. Price = " + price);
+ 			}
+ 		} );
+ 	}
+}
